1177525267f541a85c1924e151e055407410acf4
feyzaakyurek <feyzatoksal@gmail.com>
2022-03-20 22:15:50 -0400

    add regard

--- compute_tox_sent.py
+++ compute_tox_sent.py
@@ -1,16 +1,28 @@
-import json
-import pandas as pd
-from transformers import pipeline, GPT2LMHeadModel, GPT2Tokenizer
-import argparse
-import os
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from detoxify import Detoxify
-from pretrained_model_list import MODEL_LIST
-from scipy import stats
 
-API_KEY = 'AIzaSyAR1uDV9bT_qZExa_YyJe433XFoHmFnsEE'
+import pandas as pd
+import os
+import re
+import string
+from scipy import stats
+from config import parse_options
+import ipdb
+
+API_KEY = [el for el in open("perspective_key", 'r')][0]
+
+def clean_data(gen_df):
+    print("Running cleaning...")
+    # remove extra spaces and nonascii
+    def clean_line(row):
+        rr = re.sub("\s{1,}", " ", row['Generation'])
+        rr = ''.join(filter(lambda x: x in string.printable, rr))
+        return rr
+    gen_df['Generation'] = gen_df.apply(lambda x: clean_line(x), axis=1)
+    return gen_df
+
 
 def compute_hf_sentiment(gen_df):
+    print("Running HF classifier...")
+    from transformers import pipeline
     classifier = pipeline("sentiment-analysis", device=0)
     name = "Generation" if "Generation" in gen_df.columns else "Text"
     def compute_hf_sentiment_(row):
@@ -25,6 +37,8 @@ def compute_hf_sentiment(gen_df):
 
 
 def compute_vader(gen_df):
+    print("Running VADER...")
+    from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
     an = SentimentIntensityAnalyzer()
     name = "Generation" if "Generation" in gen_df.columns else "Text"
     vader_sentiments = gen_df.apply(lambda x: an.polarity_scores(x[name]), axis=1)
@@ -32,13 +46,38 @@ def compute_vader(gen_df):
     gen_df = pd.concat([gen_df, temp_vader], axis=1)
     return gen_df
     
+
 def compute_tox_detoxify(gen_df):
+    print("Running detoxify...")
+    from detoxify import Detoxify
     model = Detoxify('original', device='cuda')
     name = "Generation" if "Generation" in gen_df.columns else "Text"
     toxicities = gen_df.apply(lambda x: model.predict(x[name]), axis=1)
     temp_toxicities = pd.DataFrame(toxicities.tolist())
     gen_df = pd.concat([gen_df, temp_toxicities], axis=1)
     return gen_df
+
+
+def compute_regard(gen_df, opt):
+    print("Running regard...")
+    from nlgbias.scripts.run_classifier import regard
+    # Set filename for regard, and compute regard
+    tst = pd.read_csv(opt.test_file)
+    _, tail = os.path.split(opt.test_file)
+    tail = tail.strip(".csv")
+    newpth = os.path.join(f"nlgbias/data/regard", opt.prompt_domain + "_" + tail + ".tsv")
+    tst['Generation'].to_csv(newpth)
+
+    opt.test_file = opt.prompt_domain + "_" + tail + ".tsv"
+    preds = regard(opt)[1:]
+    gen_df['regard'] = preds
+    return gen_df
+    
+    # Now read predictions back
+    # predspth = os.path.join(opt.output_dir, tail+"_predictions.txt")
+    # preds = pd.read_csv(predspth, sep='\t', header=None)
+    # gen_df['regard'] = preds[0]
+
 
 def compute_tox_perspective(gen_df):
     client = discovery.build(
@@ -63,7 +102,6 @@ def compute_tox_perspective(gen_df):
     gen_df['perspective_api'] = gen_df.apply(lambda x: compute_single(x[name]), axis=1)
     return gen_df
         
-    
 
 def read_file(file):
     if file.endswith(".csv"):
@@ -74,28 +112,28 @@ def read_file(file):
             df = pd.DataFrame({"Text": lines})
     else:
         raise ValueError()
+    print("Finished reading the file.")
     return df
     
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser('argument for sent and tox')
-    parser.add_argument("--input_file", type=str)
-    parser.add_argument("--save_path", type=str)
-    parser.add_argument("--prompt_set", type=str, default="bold")
-    parser.add_argument("--prompt_domain", type=str, default="gender")
-    parser.add_argument("--category", type=str, default=None)
-    parser.add_argument("--summarize", action="store_true")
+
+    opt = parse_options()
     
-    
-    opt = parser.parse_args()
     domain = opt.prompt_domain
     cat = opt.category
     cat_var = "" if cat is None else f"{opt.category}_"
     
-    gen_df = read_file(opt.input_file) #f"outputs/generations/{opt.prompt_set}_{domain}_{cat}_nosampling_50000_50/gens.csv")
-#     gen_df = compute_hf_sentiment(gen_df)
-    gen_df = compute_vader(gen_df)
-    gen_df = compute_tox_detoxify(gen_df)
-#     gen_df = compute_tox_pespective(gen_df)
+    gen_df = read_file(opt.test_file) #f"outputs/generations/{opt.prompt_set}_{domain}_{cat}_nosampling_50000_50/gens.csv")
+
+    gen_df = clean_data(gen_df)
+    if opt.regard:
+        gen_df = compute_regard(gen_df, opt)
+    else:
+        # gen_df = compute_vader(gen_df)
+        # gen_df = compute_tox_detoxify(gen_df)
+        gen_df = compute_hf_sentiment(gen_df)
+        # gen_df = compute_tox_pespective(gen_df)
     
     if opt.summarize:
         mn = gen_df.groupby("Group").mean()
